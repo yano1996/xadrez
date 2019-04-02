c07898da4073e766206ca9cef485c0dc0341b733
yano1996 <marcosyano@live.com.pt>
2019-04-02 19:06:56 -0300

    Validação de campo

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,45 +8,64 @@ public class Board {
 	
 	
 	public Board(int rows, int columns) {
+		if(rows< 1 || columns < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
 	}
 
 
+	//retirado o set da linha e coluna pq o valor e fixo
 	public int getRows() {
 		return rows;
 	}
-
-
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 
 	public int getColumns() {
 		return columns;
 	}
 
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	
 	
 	public Piece piece(int row, int column) {
+		if(!positionExists(row,column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 	}
 	//Retornar a peça por posição
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[position.getRow()][position.getColum()];
 	}
 	
 	
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("Ther is already a piece on position:"+position);
+		}
 		pieces[position.getRow()][position.getColum()]= piece;
 		piece.position= position;
 	}
-		
+	//Para verificar se a posição existe
+	private boolean positionExists(int row, int column) {
+		 return row>=0 && row<rows && column >= 0 && column < columns;
+	}
 	
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(),position.getColum());
+	}
+	
+	//validação se tem uma peça esta nessa posição
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		return piece(position) != null;
+	}
 }
